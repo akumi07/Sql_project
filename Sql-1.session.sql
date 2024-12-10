176a5a9e79ff5b81be03a1d976bb5c5763d26da8
akumi07 <akash9338144396@gmail.com>
2024-12-10 22:31:28 +0530

    first commit

---
+++ Sql-1.session.sql
@@ -0,0 +1,19 @@
+-- create table command--
+DROP TABLE IF EXISTS retail_sales;
+CREATE TABLE retail_sales
+            (
+                transaction_id INT PRIMARY KEY,	
+                sale_date DATE,	 
+                sale_time TIME,	
+                customer_id	INT,
+                gender	VARCHAR(15),
+                age	INT,
+                category VARCHAR(15),	
+                quantity	INT,
+                price_per_unit FLOAT,	
+                cogs	FLOAT,
+                total_sale FLOAT
+            );
+SELECT * FROM retail_sales
+LIMIT 10
+
