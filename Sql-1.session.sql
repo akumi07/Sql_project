228b3696ec2fb089c2104772ebf9ec632d8fb1eb
akumi07 <akash9338144396@gmail.com>
2024-12-11 22:20:17 +0530

    Day 2 part-1

--- Sql-1.session.sql
+++ Sql-1.session.sql
@@ -40,3 +40,35 @@ SELECT COUNT(*) as total_sale FROM retail_sales
 
 -- How many uniuque customers we have ?
 SELECT COUNT(DISTINCT customer_id) as unique_customers FROM retail_sales
+
+SELECT COUNT(DISTINCT category) as unique_customers FROM retail_sales
+
+-- Data Analysis & Business Key Problems & Answers
+
+-- My Analysis & Findings
+-- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
+-- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
+-- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
+-- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
+-- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
+-- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
+-- Q.8 Write a SQL query to find the top 5 customers based on the highest total sales 
+-- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
+-- Q.10 Write a SQL query to create each shift and number of orders (Example Morning <=12, Afternoon Between 12 & 17, Evening >17)
+
+Select * from retail_sales
+where sale_date='2022-11-05'
+
+SELECT *
+FROM retail_sales
+WHERE category = 'Clothing'
+  AND DATE_FORMAT(sale_date, '%Y-%m') = '2022-11'
+  AND quantity >= 4;
+
+
+select category , Sum(total_sale)as net_sale
+from retail_sales
+GROUP BY category
+
+
